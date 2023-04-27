e83ac78d87b46e6a402161e6c4673718d1e26a50
jmosakowski <jmosakowski@BR-WYK>
2023-04-27 18:25:51 +0200

    Dodaj pliki projektów.

---
+++ Ex_05_01/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;               // needed for AppContext
+using System.IO;            // needed for File
+using System.Windows;       // needed for RoutedEventArgs, Window
+using Microsoft.Win32;      // needed for OpenFileDialog
+
+namespace Ex_05_01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuFileLoadPath_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void MenuFileSavePath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuFileExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuInfoAbout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
